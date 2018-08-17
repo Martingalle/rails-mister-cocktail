62a92ae8e282837670633d61fa6fa96643f4a030
Martin Descours <whoismartin@hotmail.com>
2018-08-17 17:13:00 +0200

    remove javascript

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_17_141321) do
+ActiveRecord::Schema.define(version: 2018_08_17_145550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2018_08_17_141321) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "cocktail_id"
+    t.integer "rating"
     t.index ["cocktail_id"], name: "index_reviews_on_cocktail_id"
   end
 
